6879ba8da2639714130808af6f3229949cd92c2f
JohnniePro <toshigamer21@gmail.com>
2025-06-12 08:59:28 +0300

    Fix: removed double header problem

--- components/layout/dashboard-layout.tsx
+++ components/layout/dashboard-layout.tsx
@@ -13,7 +13,7 @@ export function DashboardLayout({
     <div className="flex h-screen bg-gray-50">
       <AdminSidebar />
       <div className="flex flex-1 flex-col overflow-hidden">
-        <Header title={title} />
+        <Header title={""} />
         <main className="flex-1 overflow-auto p-6">
           <div className="mx-auto max-w-7xl space-y-6">{children}</div>
         </main>
